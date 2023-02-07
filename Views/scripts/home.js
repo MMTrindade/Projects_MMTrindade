cb5026a199cd6152a3128774f3e8940bcc7f2325
MMTrindade <matheus.tri.2010@gmail.com>
2023-02-07 17:58:54 +0000

    Sobre

---
+++ Views/scripts/home.js
@@ -0,0 +1,5 @@
+document.getElementById('SaibaMais').addEventListener('click', function () {
+    document.querySelector('.PropositoTitle').toggleClass('animate-appearHome');
+    document.querySelector('.PropositoTexto').toggleClass('animate-left');
+    document.querySelector('.PropositoImagem').toggleClass('animate-right');
+  });
